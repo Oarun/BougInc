aa1f91b03e632595c4230d399dc62dc3f130c62a
Evgeny Gubin <102744819+Oarun@users.noreply.github.com>
2024-02-08 23:09:19 -0800

    Added identity and migration. Create user secrets for CulturnaryConnection with the connection string so there is no confusion with json connection string

--- src/CulturNary/CulturNary.Web/Program.cs
+++ src/CulturNary/CulturNary.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using CulturNary.Web.Areas.Identity.Data;
 namespace CulturNary.Web;
 
 public class Program
@@ -5,10 +8,15 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        var connectionString = builder.Configuration.GetConnectionString("CulturnaryConnection") ?? throw new InvalidOperationException("Connection string not found.");
+
+        builder.Services.AddDbContext<IdentityDataContext>(options => options.UseSqlServer(connectionString));
+
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<IdentityDataContext>();
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
-
+        
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -24,6 +32,7 @@ public class Program
 
         app.UseRouting();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
         app.MapControllerRoute(
