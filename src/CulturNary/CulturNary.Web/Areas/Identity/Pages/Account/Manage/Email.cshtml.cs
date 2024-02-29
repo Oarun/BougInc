b3220ba81ddd7469fd35f528e8adcdfb2519cfc8
LunafreyjaS <lschafer21@mail.wou.edu>
2024-02-29 14:59:04 -0800

    Updated new scaffolds to use SiteUser

--- src/CulturNary/CulturNary.Web/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ src/CulturNary/CulturNary.Web/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -12,18 +12,19 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using CulturNary.Web.Areas.Identity.Data;
 
 namespace CulturNary.Web.Areas.Identity.Pages.Account.Manage
 {
     public class EmailModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<SiteUser> _userManager;
+        private readonly SignInManager<SiteUser> _signInManager;
         private readonly IEmailSender _emailSender;
 
         public EmailModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager,
+            UserManager<SiteUser> userManager,
+            SignInManager<SiteUser> signInManager,
             IEmailSender emailSender)
         {
             _userManager = userManager;
@@ -73,7 +74,7 @@ namespace CulturNary.Web.Areas.Identity.Pages.Account.Manage
             public string NewEmail { get; set; }
         }
 
-        private async Task LoadAsync(IdentityUser user)
+        private async Task LoadAsync(SiteUser user)
         {
             var email = await _userManager.GetEmailAsync(user);
             Email = email;
--- src/CulturNary/CulturNary.Web/Areas/Identity/Pages/Account/Manage/ResetAuthenticator.cshtml.cs
+++ src/CulturNary/CulturNary.Web/Areas/Identity/Pages/Account/Manage/ResetAuthenticator.cshtml.cs
@@ -8,18 +8,19 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using CulturNary.Web.Areas.Identity.Data;
 
 namespace CulturNary.Web.Areas.Identity.Pages.Account.Manage
 {
     public class ResetAuthenticatorModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<SiteUser> _userManager;
+        private readonly SignInManager<SiteUser> _signInManager;
         private readonly ILogger<ResetAuthenticatorModel> _logger;
 
         public ResetAuthenticatorModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager,
+            UserManager<SiteUser> userManager,
+            SignInManager<SiteUser> signInManager,
             ILogger<ResetAuthenticatorModel> logger)
         {
             _userManager = userManager;
