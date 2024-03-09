ea60804342bc376c47a47b3f003d55c35f5c8e33
Evgeny Gubin <102744819+Oarun@users.noreply.github.com>
2024-03-09 01:52:49 -0800

    Added collections to the modal, also fixed some errors with signed role, forgot to write code for adding the role in program.cs

--- src/CulturNary/CulturNary.Web/wwwroot/js/ModalAdmin.js
+++ src/CulturNary/CulturNary.Web/wwwroot/js/ModalAdmin.js
@@ -12,10 +12,40 @@ $(document).ready(function() {
                 $('#normalizedUserName').text(user.normalizedUserName);
                 $('#email').text(user.email);
                 $('#normalizedEmail').text(user.normalizedEmail);
-                $('#emailConfirmed').text(user.emailConfirmed ? 'Yes' : 'No');
-                $('#phoneNumber').text(user.phoneNumber ? user.phoneNumber : 'N/A');
-                $('#twoFactorEnabled').text(user.twoFactorEnabled ? 'Yes' : 'No');
+                $('#emailConfirmed').text(user.emailConfirmed);
+                $('#phoneNumber').text(user.phoneNumber || "N/A" );
+                $('#twoFactorEnabled').text(user.twoFactorEnabled);
 
+                // Fetch person details
+                return fetch('/api/Person/Identity/' + userId);
+            })
+            .then(response => response.json())
+            .then(person => {
+                // Check if person.id is defined before fetching collections
+                if (person.id) {
+                    return fetch('/api/Collection/' + person.id);
+                } else {
+                    throw new Error('PersonId is undefined');
+                }
+            })
+            .then(response => response.json())
+            .then(collections => {
+                // Check if collections is an array before iterating over it
+                if (Array.isArray(collections)) {
+                    // Populate the collections
+                    var collectionsList = $('#collections');
+                    collectionsList.empty();
+                    var ul = $('<ul>');
+                    collections.forEach(collection => {
+                        var listItem = $('<li>');
+                        listItem.text(collection.name + ': ' + (collection.description || 'N/A'));
+                        ul.append(listItem);
+                    });
+                    collectionsList.append(ul);
+                } else {
+                    throw new Error('collections is not an array');
+                }
+            
                 // Show the modal
                 $('#myModal').modal('show');
             })
