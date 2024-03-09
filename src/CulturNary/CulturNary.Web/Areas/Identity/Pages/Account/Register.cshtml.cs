5532b3cd48a2a659b75e35c065995ddb4ac9db79
Evgeny Gubin <102744819+Oarun@users.noreply.github.com>
2024-03-08 22:15:32 -0800

    Added restriction for user who is logged into access registration and login

--- src/CulturNary/CulturNary.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/CulturNary/CulturNary.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -115,10 +115,14 @@ namespace CulturNary.Web.Areas.Identity.Pages.Account
         }
 
 
-        public async Task OnGetAsync(string returnUrl = null)
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
         {
+            if(User.Identity.IsAuthenticated){
+                return RedirectToPage("/Index");
+            }
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            return Page();
         }
         public bool AuthorizationKeyword(string keyword){
             var adminKeyword = Environment.GetEnvironmentVariable("ADMIN_KEYWORD");
