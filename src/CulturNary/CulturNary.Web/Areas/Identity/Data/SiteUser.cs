3bc16e84c59de0e3750ac4fa2ef5de59daf31a71
LunafreyjaS <lschafer21@mail.wou.edu>
2024-04-06 16:29:44 -0700

    SiteUser Migration + WIP pages

--- src/CulturNary/CulturNary.Web/Areas/Identity/Data/SiteUser.cs
+++ src/CulturNary/CulturNary.Web/Areas/Identity/Data/SiteUser.cs
@@ -1,16 +1,47 @@
+using System.Security.Policy;
+using System.Text.Json;
 using Microsoft.AspNetCore.Identity;
 
 namespace CulturNary.Web.Areas.Identity.Data
 {
     public class SiteUser : IdentityUser
     {
+
         [PersonalData]
         public string? DisplayName { get; set; }
         [PersonalData]
         public string? Biography { get; set; }
         [PersonalData]
         public string? ProfileImageName { get; set; }
+        [PersonalData]
+        public string? UserLikes { get; set; }
+        [PersonalData]
+        public string? UserDislikes { get; set; }
+        [PersonalData]
+        public string? DietaryRestrictions { get; set; }
 
+        public List<DietaryRestriction> GetDietaryRestrictions()
+        {
+            if (string.IsNullOrEmpty(DietaryRestrictions))
+            {
+                return new List<DietaryRestriction>();
+            }
+            else
+            {
+                return JsonSerializer.Deserialize<List<DietaryRestriction>>(DietaryRestrictions);
+            }
+        }
+
+        public void SetDietaryRestrictions(List<DietaryRestriction> restrictions)
+        {
+            DietaryRestrictions = JsonSerializer.Serialize(restrictions);
+        }
+    }
+
+    public class DietaryRestriction
+    {
+        public string? Name { get; set; }
+        public bool Active { get; set; }
     }
 
 }
---
+++ src/CulturNary/CulturNary.Web/Areas/Identity/Pages/Account/Manage/EditLikesDislikes.cshtml.cs
@@ -0,0 +1,131 @@
+// // Licensed to the .NET Foundation under one or more agreements.
+// // The .NET Foundation licenses this file to you under the MIT license.
+// #nullable disable
+
+// using System;
+// using System.ComponentModel.DataAnnotations;
+// using System.Text.Encodings.Web;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Identity;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Mvc.RazorPages;
+// using CulturNary.Web.Areas.Identity.Data;
+// using CulturNary.Web.Data.Migrations;
+
+// namespace CulturNary.Web.Areas.Identity.Pages.Account.Manage
+// {
+//     public class LikeDislikeModel : PageModel
+//     {
+//         private readonly UserManager<SiteUser> _userManager;
+//         private readonly SignInManager<SiteUser> _signInManager;
+
+//         public LikeDislikeModel(
+//             UserManager<SiteUser> userManager,
+//             SignInManager<SiteUser> signInManager)
+//         {
+//             _userManager = userManager;
+//             _signInManager = signInManager;
+//         }
+
+//         public string Likes { get; set; }
+
+//         public string Dislikes { get; set; }
+
+//         /// <summary>
+//         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+//         ///     directly from your code. This API may change or be removed in future releases.
+//         /// </summary>
+//         public string Username { get; set; }
+
+//         /// <summary>
+//         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+//         ///     directly from your code. This API may change or be removed in future releases.
+//         /// </summary>
+//         [TempData]
+//         public string StatusMessage { get; set; }
+
+//         /// <summary>
+//         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+//         ///     directly from your code. This API may change or be removed in future releases.
+//         /// </summary>
+//         [BindProperty]
+//         public InputModel Input { get; set; }
+
+//         /// <summary>
+//         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+//         ///     directly from your code. This API may change or be removed in future releases.
+//         /// </summary>
+//         public class InputModel
+//         {
+//             /// <summary>
+//             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+//             ///     directly from your code. This API may change or be removed in future releases.
+//             /// </summary>
+//             [Display(Name = "Likes")]
+//             public string likes { get; set; }
+
+//             [Display(Name = "Dislikes")]
+//             public string dislikes { get; set; }
+//         }
+
+//         private async Task LoadAsync(SiteUser user)
+//         {
+//             var userName = await _userManager.GetUserNameAsync(user);
+//             var userData = await _userManager.GetUserAsync(User);
+//             Biography = userData.Biography;
+
+//             Username = userName;
+
+//             Input = new InputModel
+//             {
+//                 biography = Biography
+//             };
+//         }
+
+//         public async Task<IActionResult> OnGetAsync()
+//         {
+//             var user = await _userManager.GetUserAsync(User);
+//             if (user == null)
+//             {
+//                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+//             }
+
+//             await LoadAsync(user);
+//             return Page();
+//         }
+
+//         public async Task<IActionResult> OnPostAsync()
+//         {
+//             var user = await _userManager.GetUserAsync(User);
+//             if (user == null)
+//             {
+//                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+//             }
+
+//             if (!ModelState.IsValid)
+//             {
+//                 await LoadAsync(user);
+//                 return Page();
+//             }
+
+
+//             var userData = await _userManager.GetUserAsync(User);
+//             var biography = userData.Biography;
+
+//             if (Input.biography != biography && !string.IsNullOrEmpty(Input.biography))
+//             {
+//                 user.Biography = Input.biography;
+//                 var updateBiographyResult = await _userManager.UpdateAsync(user);
+//                 if (!updateBiographyResult.Succeeded)
+//                 {
+//                     StatusMessage = "Unexpected error when trying to edit your biography.";
+//                     return RedirectToPage();
+//                 }
+//                 StatusMessage = "Your biography has been updated";
+//             }
+
+//             await _signInManager.RefreshSignInAsync(user);
+//             return RedirectToPage();
+//         }
+//     }
+// }
